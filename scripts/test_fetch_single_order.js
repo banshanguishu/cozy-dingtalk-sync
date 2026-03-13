e6a5360a1100ecbef20ab5e8f3dd4e21fd7e6228
goubo42@163.com <goubo42@163.com>
2026-03-13 08:55:22 +0800

    chore: pnpm-lock.yaml

--- scripts/test_fetch_single_order.js
+++ scripts/test_fetch_single_order.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 // 在此处替换为您要查询的订单 ID
 // const ORDER_ID = "gid://shopify/Order/6778719469886"; // 3726
 // const ORDER_ID = "gid://shopify/Order/6792919646526"; // 3851
-const ORDER_ID = "gid://shopify/Order/6796155322686";
+const ORDER_ID = "gid://shopify/Order/6811243774270";
 
 const { SHOPIFY_STORE_URL, SHOPIFY_ADMIN_API_ACCESS_TOKEN, SHOPIFY_API_VERSION } = process.env;
 
